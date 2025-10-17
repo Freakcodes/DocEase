9fd3f482312e6a4efbc742e353dcdf95465d9787
Freakcodes <gabhay408@gmail.com>
2025-10-17 13:38:03 +0530

    fixing cors

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,9 @@ app.use(
   })
 );
 //api endpoints
-app.use("/api/admin", adminRouter);
-app.use("/api/doctor",doctorRouter);
-app.use("/api/user",userRouter)
+app.use("api/admin", adminRouter);
+app.use("api/doctor",doctorRouter);
+app.use("api/user",userRouter)
 
 app.get("/", (req, res) => {
   res.send("API WORKING FINE");
